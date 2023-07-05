c51ebe1b32d9a40a5532f2fbab6fa868943b8437
ThibautSembeni <tsembeni@myges.fr>
2023-07-05 20:00:04 +0200

    fix some bugs

--- client/src/views/PaymentsView.vue
+++ client/src/views/PaymentsView.vue
@@ -56,8 +56,6 @@ const filteredPaymentsAll = computed(() => {
   <div class="sm:ml-64">
     <NavBar />
     <div class="p-4 lg:p-10">
-      <h1 class="text-3xl font-bold">Paiements</h1>
-
       <h1 class="text-3xl font-bold"><i class="fa-solid fa-dollar-sign mr-2"></i> Paiements</h1>
 
       <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200">
@@ -178,99 +176,6 @@ const filteredPaymentsAll = computed(() => {
             </tr>
           </template>
         </Table>
-      </div>
-
-      <div class="relative overflow-x-auto" v-if="data.currentTab === 'Tous les paiements'">
-        <div class="flex flex-wrap my-3">
-          <button
-            v-for="filter in data.filtersAll"
-            :key="filter"
-            @click="data.currentFilterAll = filter"
-            :class="`relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br focus:outline-none transition duration-150 ease-in-out ${
-              filter === data.currentFilterAll
-                ? 'from-green-400 to-blue-600'
-                : 'hover:from-green-400 hover:to-blue-600 from-gray-400 to-gray-400'
-            }`"
-          >
-            <span
-              class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md"
-            >
-              {{ filter }}
-            </span>
-          </button>
-        </div>
-
-        <table class="w-full text-sm text-left text-gray-500">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 border-b">
-            <tr>
-              <th scope="col" class="px-6 py-3">Montant</th>
-              <th scope="col" class="px-6 py-3">Description</th>
-              <th scope="col" class="px-6 py-3">Client</th>
-              <th scope="col" class="px-6 py-3">Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="payment in filteredPaymentsAll" :key="payment.id" class="bg-white border-b">
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                <router-link :to="{ name: 'paymentDetail', params: { uuid: 'a_remplace' } }">
-                  {{ payment.amount }}
-                  <span class="ml-4 font-light text-gray-400">{{ payment.currency }}</span>
-                  <span
-                    :class="`text-sm font-medium mr-2 px-2.5 py-0.5 rounded ml-4 ${
-                      payment.status === 'Réussi'
-                        ? 'bg-green-100 text-green-800'
-                        : 'bg-red-100 text-red-800'
-                    } `"
-                    >{{ payment.status }}</span
-                  >
-                </router-link>
-              </th>
-              <td class="px-6 py-4">
-                {{ payment.description }}
-              </td>
-              <td class="px-6 py-4">###</td>
-              <td class="px-6 py-4">
-                {{ payment.date }}
-              </td>
-            </tr>
-            <tr class="bg-white border-b" v-if="!filteredPaymentsAll">
-              <th
-                colspan="4"
-                scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center"
-              >
-                Aucun paiement
-              </th>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-
-      <div class="relative overflow-x-auto" v-if="data.currentTab === 'Toutes les transactions'">
-        <table class="w-full text-sm text-left text-gray-500">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 border-b">
-            <tr>
-              <th scope="col" class="px-6 py-3">Type</th>
-              <th scope="col" class="px-6 py-3">Net</th>
-              <th scope="col" class="px-6 py-3">Montant</th>
-              <th scope="col" class="px-6 py-3">Frais</th>
-              <th scope="col" class="px-6 py-3">Description</th>
-              <th scope="col" class="px-6 py-3">Disponible le</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr class="bg-white border-b">
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                Paiement
-              </th>
-              <td class="px-6 py-4">208 975,98 €</td>
-              <td class="px-6 py-4">200 000,00 €</td>
-              <td class="px-6 py-4">(8 975,98 €) EUR</td>
-              <td class="px-6 py-4">Achat par : Romain Lethumier</td>
-              <td class="px-6 py-4">4 mai</td>
-            </tr>
-          </tbody>
-        </table>
       </div>
     </div>
   </div>
