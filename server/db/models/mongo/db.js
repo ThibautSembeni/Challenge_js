b8702c501f41f7229b8edfbd86401d6f80ffa324
ThibautSembeni <tsembeni@myges.fr>
2023-07-08 13:35:26 +0200

    handle mongo conection for test

--- server/db/models/mongo/db.js
+++ server/db/models/mongo/db.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('\u001b[' + 32 + 'm' + `connected to ${process.env.MONGO_URL.split(":")[0]} database` + '\u001b[0m'))
-    .catch((err) => console.log(err))
-
+let connection;
+if (process.env.NODE_ENV === 'test') {
+    connection = mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+} else {
+    connection = mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('\u001b[' + 32 + 'm' + `connected to ${process.env.MONGO_URL.split(":")[0]} database` + '\u001b[0m'))
+        .catch((err) => console.log(err))
+}
 module.exports = connection;
+
