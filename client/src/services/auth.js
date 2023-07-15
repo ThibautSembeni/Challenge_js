95d384c1c4750ebf08b95a3436bb76c25edb0e01
Ossama9 <oussama.dahbi98@gmail.com>
2023-07-16 01:07:55 +0200

    add register method for RegisterView.vue

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -2,21 +2,31 @@ import store from '@/stores/store'
 import httpClient from '@/services/httpClient'
 
 export async function login(userCredentials) {
-    try {
-        const response = await httpClient.post('/login', userCredentials)
-        if (response.status === 200) {
-            const {token} = response.data
-            localStorage.setItem('access_token', token)
-            store.commit('setLoggedIn', true)
-            return token
-        } else if (response.status === 422) {
-            throw new Error(response.data)
-        } else if (response.status === 401) {
-            throw new Error("Le mot de passe ou l'adresse email ne sont pas corrects")
-        }
-    } catch (error) {
-        throw new Error("Une erreur s'est produite lors de la connexion")
+    const response = await httpClient.post('/login', userCredentials)
+    if (response.status > 401) {
+        throw new Error(response.data)
+    } else if (response.status === 401) {
+        throw new Error("Le mot de passe ou l'adresse email ne sont pas corrects")
     }
+    const {token} = response.data
+    localStorage.setItem('access_token', token)
+    store.commit('setLoggedIn', true)
+    return token
+
+
+}
+
+export async function registerUser(userCredentials) {
+    const response = await httpClient.post('/register', userCredentials)
+    if (response.status === 422) {
+        const errorData = response.data;
+        throw new Error(`Invalid registration data: ${errorData.message}`);
+    } else if (response.status === 409) {
+        const errorMessage = `L'adresse email ${userCredentials.email} est déjà utilisée.`;
+        throw new Error(errorMessage);
+    }
+    return response;
+
 }
 
 export async function getCurrentUser() {
