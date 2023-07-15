d35422efb0a109305fae7e6c356db6ccf346b83b
Ossama9 <oussama.dahbi98@gmail.com>
2023-07-15 19:42:20 +0200

    add route /admin/merchants

--- server/routes/merchant.js
+++ server/routes/merchant.js
@@ -13,10 +13,10 @@ module.exports = new genericRouter(
     {
         // `customRoutes` is optional is for add others custom routes
         customRoutes: [
-            { handler: 'countPendingUsers', method: 'get', path: '/pending/count', middleware: [checkAdminRole] },
-            { handler: 'getPendingValidationMerchants', method: 'get', path: '/pending', middleware: [checkAdminRole] },
-            { handler: 'approveMerchant', method: 'get', path: '/approve/:id', middleware: [checkAdminRole] },
-            { handler: 'declineMerchant', method: 'get', path: '/decline/:id', middleware: [checkAdminRole] },
+            { handler: 'countPendingUsers', method: 'get', path: '/merchants/pending/count', middleware: [checkAdminRole] },
+            { handler: 'getPendingValidationMerchants', method: 'get', path: '/merchants/pending', middleware: [checkAdminRole] },
+            { handler: 'approveMerchant', method: 'get', path: '/merchants/approve/:id', middleware: [checkAdminRole] },
+            { handler: 'declineMerchant', method: 'get', path: '/merchants/decline/:id', middleware: [checkAdminRole] },
         ],
         defaultRoutes: {},
         // `middlewares` is for set a middleware on all routes over
