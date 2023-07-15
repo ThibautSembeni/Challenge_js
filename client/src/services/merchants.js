9275b82a56030b26bb504051dedefb45570024cd
Ossama9 <oussama.dahbi98@gmail.com>
2023-07-16 01:46:48 +0200

    commit for reset

--- client/src/services/merchants.js
+++ client/src/services/merchants.js
@@ -1,12 +1,12 @@
 import httpClient from '@/services/httpClient'
 
 export async function getPendingMerchants() {
-	const response = await httpClient.get('/admin/merchants/pending');
-	if (response.status === 200) {
-		return response.data;
-	} else {
-		throw new Error(`Error: ${response.status} - Une erreur s'est produite lors de l'approbation du marchand`);
-	}
+    const response = await httpClient.get('/admin/merchants/pending');
+    if (response.status === 200) {
+        return response.data;
+    } else {
+        throw new Error(`Error: ${response.status} - Une erreur s'est produite lors de l'approbation du marchand`);
+    }
 }
 
 export async function approveMerchant(id) {
