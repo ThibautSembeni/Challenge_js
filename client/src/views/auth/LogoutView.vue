75eda43701c5409e79f4eb660044b316d4eed8e0
Ossama9 <oussama.dahbi98@gmail.com>
2023-07-15 23:59:38 +0200

    update page logout

--- client/src/views/auth/LogoutView.vue
+++ client/src/views/auth/LogoutView.vue
@@ -1,12 +1,22 @@
 <script setup>
 import store from "@/stores/store";
 
-store.commit('setUser',null)
-store.commit('setLoggedIn',false)
+store.commit('setUser', null)
+store.commit('setLoggedIn', false)
 </script>
 <template>
   <div>
     logout
+  </div>
+  <br>
+  <br>
+  <br>
+  <div>
+
+    <RouterLink to="login">
+
+      Go pour login page
+    </RouterLink>
   </div>
 </template>
 
