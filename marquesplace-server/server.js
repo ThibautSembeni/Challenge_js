aefb62c78a6845a6e378fe614e9017baf3c2b4d0
Ossama9 <oussama.dahbi98@gmail.com>
2023-07-26 19:51:04 +0200

    middelware oK

--- marquesplace-server/server.js
+++ marquesplace-server/server.js
@@ -27,26 +27,27 @@ app.use(express.json());
 
 app.use("/", SecurityRouter);
 
+app.get('/transactions', async (req, res, next) => {
+  const [statusCode, data] = await app.getAllTransaction()
+  res.sendStatus(statusCode)
+})
+
+app.post('/transactions', async (req, res, next) => {
+  const [statusCode, data] = await app.createTransaction(req.body)
+  res.sendStatus(statusCode).json(data)
+})
+
 
 app.use("/cart", checkAuth, CartRouter);
 
 const errorHandler = (err, req, res, next) => {
-  // Assurez-vous que l'objet d'erreur existe
   if (!err) {
     return next();
   }
-
-  // Affichez l'erreur dans la console pour le débogage
   console.error(err);
-
-  // Vérifiez si l'erreur a un code HTTP personnalisé (dans l'objet d'erreur)
   const statusCode = err.statusCode || 500;
-
-  // Réponse avec l'erreur et le statut approprié
   res.status(statusCode).json({ error: err.message || 'Internal Server Error' });
 };
-
-// Utilisez le middleware errorHandler pour gérer toutes les erreurs
 
 
 app.get("/", (req, res) => {
