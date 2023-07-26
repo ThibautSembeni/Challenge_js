0a78a29a3b3cb4c1da6fc4f2bc584054928f314c
mchemlal <michel.chemlal@gmail.com>
2023-07-26 16:52:01 +0200

    ajout fixtures products fixs a faire

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,28 +1,30 @@
-const {faker} = require('@faker-js/faker');
+const { faker } = require('@faker-js/faker');
 const { QueryTypes } = require('sequelize');
 const db = require('./db/models/postgres');
 
-const roles = ['admin', 'merchant', 'customer'];
+
 
 const generateRandomUser = async () => {
-    const firstname = faker.person.firstName();
-    const lastname = faker.person.lastName();
-    const company = faker.company.buzzPhrase();
-    const email = faker.internet.email();
-    const password = faker.internet.password();
-    const role = roles[Math.floor(Math.random() * roles.length)];
+    const product_name = faker.commerce.productName();
+    const description = faker.commerce.productDescription();
+    const price = faker.commerce.price();
+    const reference = faker.string.alphanumeric(15);
+    const stock = faker.string.numeric(2);
+    const status = "active"
     const createdAt = faker.date.past();
     const updatedAt = faker.date.recent();
+    const merchantId = 1
 
-    await db.User.create({
-        firstname,
-        lastname,
-        company,
-        email,
-        password,
-        role,
+    await db.Product.create({
+        product_name,
+        description,
+        price,
+        reference,
+        stock,
+        status,
         createdAt,
         updatedAt,
+        merchantId
     });
 };
 
@@ -30,7 +32,7 @@ const fixturesLoader = async () => {
     try {
         //await db.sync(); // Sync the database
 
-        for (let i = 0; i < 50; i++) {
+        for (let i = 0; i < 20; i++) {
             await generateRandomUser();
         }
 
