2516af20392c56c9a6ea0610ef842b43c280fb3b
mchemlal <michel.chemlal@gmail.com>
2023-07-26 14:48:09 +0200

    fixtures user adapter pour random role fois 50

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,73 +1,45 @@
-const { faker } = require('@faker-js/faker');
+const {faker} = require('@faker-js/faker');
 const { QueryTypes } = require('sequelize');
 const db = require('./db/models/postgres');
 
-// Generate a random name
-const firstname = faker.person.firstName();
+const roles = ['admin', 'merchant', 'customer'];
 
-const lastname = faker.person.firstName();
+const generateRandomUser = async () => {
+        const firstname = faker.person.firstName();
+        const lastname = faker.person.lastName();
+        const company = faker.company.buzzPhrase();
+        const email = faker.internet.email();
+        const password = faker.internet.password();
+        const role = roles[Math.floor(Math.random() * roles.length)];
+        const createdAt = faker.date.past();
+        const updatedAt = faker.date.recent();
 
-const company = faker.company.buzzPhrase();
-
-// Generate a random email address
-const email = faker.internet.email();
-
-const password = faker.internet.password();
-
-const role = 'admin';
-
-const clientInfo = {
-        name: faker.person.fullName(),
-        email: faker.internet.email(),
-        phoneNumber: faker.phone.number(),
-
-}
-
-
-
-const randomBillingInfo = {
-    address: faker.location.streetAddress(),
-    city: faker.location.city(),
-    postalCode: faker.location.zipCode(),
-    country: faker.location.country(),
+        await db.User.create({
+                firstname,
+                lastname,
+                company,
+                email,
+                password,
+                role,
+                createdAt,
+                updatedAt,
+        });
 };
-
-const randomShippingInfo = {
-    address: faker.location.streetAddress(),
-    city: faker.location.city(),
-    postalCode: faker.location.zipCode(),
-    country: faker.location.country(),
-};
-
-const randomCart = [
-    { name: faker.commerce.product(), price: faker.string.numeric() },
-    { name: faker.commerce.product(), price: faker.string.numeric() },
-];
-//
-const randomAmount = faker.string.numeric();
-const randomCurrency = 'eur';
-const randomStatus = 'paid';
-const randomReference = faker.string.alphanumeric(20);
-const createdAt = faker.date.past();
-const updatedAt = faker.date.recent();
-
-
-
-/*const db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    logging: true,
-});*/
-
-/*db.query('INSERT INTO users (lastname, firstname, company, email, password, role, createdAt, updatedAt) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
-    {
-    bind: [lastname, firstname, company, email, password, role, randomCreatedAt, randomUpdatedAt], type: QueryTypes.INSERT
-    });*/
 
 const fixturesLoader = async () => {
-        await db.User.create({ lastname, firstname, company, email, password, role });
-        await db.Transaction.create({clientInfo, randomCart, randomShippingInfo ,randomBillingInfo, randomAmount, randomCurrency, randomStatus, randomReference, createdAt, updatedAt });
+        try {
+                //await db.sync(); // Sync the database
 
-        process.exit(1)
-}
+                for (let i = 0; i < 50; i++) {
+                        await generateRandomUser();
+                }
 
-fixturesLoader()
+                console.log('50 random users created successfully!');
+        } catch (error) {
+                console.error('Error creating users:', error);
+        } finally {
+                process.exit(1);
+        }
+};
+
+fixturesLoader();
