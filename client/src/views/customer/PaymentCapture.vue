8885beb7ecaceb9ce58c5fa160ef7ead0d09601b
Ossama9 <oussama.dahbi98@gmail.com>
2023-07-27 09:58:45 +0200

    clean page

--- client/src/views/customer/PaymentCapture.vue
+++ client/src/views/customer/PaymentCapture.vue
@@ -3,36 +3,12 @@ import {useRoute} from "vue-router";
 import {onMounted, reactive, ref} from "vue";
 // import { getTransaction} from "@/services/transactions";
 import GenericButton from "@/components/GenericButton.vue";
-import Form from "@/components/form/Form.vue";
 // import {cancelTransaction, createOperation} from "@/services/operations";
-import Summary from "@/components/Summary.vue";
 
 const route = useRoute()
 
 const transactionReference = route.params.reference
 const transaction = ref(null)
-
-const items = [
-  {
-    title: "Product 1",
-    description: "lorem impsu liwe",
-    price: 550,
-    img: "https://picsum.photos/id/1005/600/200"
-  },
-  {
-    title: "Product 2",
-    description: "lorem impsu liwe",
-    price: 250,
-    img: "https://picsum.photos/id/1005/600/200"
-  },
-  {
-    title: "Product 3",
-    description: "lorem impsu liwe",
-    price: 150,
-    img: "https://picsum.photos/id/1005/600/200"
-  }
-]
-
 
 // const submitForm = async (formData) => {
 //   const amount = transaction.value.amount
@@ -58,12 +34,12 @@ const eventHandler = (cardData) => {
   <div class="flex">
 
     <div class="flex flex-col justify-center flex-1 mx-auto">
-      <Summary :items="items"/>
+
     </div>
-    <form class="flex-1">
+    <div class="flex-1">
       <CreditCard @test="eventHandler"/>
-      <input label="" type="hidden" :value="transactionReference" name="transactionReference" />
-    </form>
+      <input type="hidden" :value="transactionReference" name="transactionReference" />
+    </div>
 
   </div>
 </template>
