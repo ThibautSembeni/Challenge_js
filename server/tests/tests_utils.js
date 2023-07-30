a6f7e58e2d2e510b5bf286030b005ae966ffd66a
Ossama9 <oussama.dahbi98@gmail.com>
2023-07-30 13:55:48 +0200

    add tests_utils

---
+++ server/tests/tests_utils.js
@@ -0,0 +1,13 @@
+function generateRandomString(length=5) {
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters.charAt(randomIndex);
+    }
+    return result;
+}
+
+module.exports = {
+    generateRandomString
+};
